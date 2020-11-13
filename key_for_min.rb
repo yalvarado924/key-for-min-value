080ba02e0cc3e4a56971b318cc0b0b1a7416c712
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-13 08:40:38 -0600

    commit tree 4d73a966f1a7681825de922c1706e3dcbf64596e

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,12 @@
 
 def key_for_min_value(name_hash)
 
+
+    min_key,min_value = name_hash.first
+    name_hash.collect do |key, value|
+        if value < min_value
+            min_key = key
+        end
+    end
+    min_key
 end
